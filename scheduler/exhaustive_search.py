0e189e97463e403c0ce70f05a149e94c8d06a6c4
do-dong-park <rnlgksclsrn9868@gmail.com>
2024-07-03 18:27:47 +0900

    🎨 병렬 가능한 작업 뽑아내는 중

--- scheduler/exhaustive_search.py
+++ scheduler/exhaustive_search.py
@@ -76,7 +76,7 @@ class ExhaustiveSearch:
 
             return task_end_time
 
-        def handle_constraints(subtask: Subtask, makespan: int, index: int) -> int:
+        def handle_Waiting(subtask: Subtask, makespan: int, index: int) -> int:
             """작업 시작 조건 충족위한 대기시간 결정
 
             Args:
@@ -106,7 +106,7 @@ class ExhaustiveSearch:
                         # 만약, 현 시점이 제약 조건을 충족하였다면
                         return makespan  # No waiting needed
                     else:
-                        # 제약 조건을 충족하지 않아, Surveilance
+                        # 제약 조건을 충족하지 않는 경우, Waiting
                         waiting_time = task_end_time + constraint_duration - makespan
                         log[f"{constraint_type}_for_{constraint_task}_{index}"] = {
                             "Start": makespan,
@@ -119,13 +119,49 @@ class ExhaustiveSearch:
                     )
             return makespan
 
-        # parallel_tasks = self.find_parallel_tasks(permutation, subtask.location)
-        #         for parallel_task in parallel_tasks:
-        #             permutation.remove(parallel_task)
-        #             makespan = self.update_log_and_makespan(
-        #                 parallel_task, surveillance_start_time, log, index
-        #             )
-        #             surveillance_start_time = makespan
+        def handle_Surveillance(subtask: Subtask, makespan: int, index: int) -> int:
+            """작업 시작 조건 충족위한 대기시간 결정
+
+            Args:
+                subtask (Subtask): 시작에 시간 제약이 있는 작업
+                makespan (int): 작업 시작 전 시각
+                index (int): dict key 겹침 방지용
+
+            Raises:
+                ValueError: Constraint가 없는 작업이 해당 함수에 들어온 경우
+
+            Returns:
+                int: 해당 작업 완료 시각
+            """
+            constraint_task = subtask.constraints.get("After")
+            constraint_duration = subtask.constraints.get("Duration", 0)
+            constraint_type = subtask.constraints.get("Type", 0)
+
+            if constraint_task:
+                constraint_key = next(
+                    (key for key in log if key.startswith(constraint_task)), None
+                )
+                # constraint_key : 제약 조건 기준 작업
+                if constraint_key:
+                    task_end_time = log[constraint_key]["End"]
+
+                    # TODO 병렬화 가능한 작업을 동일한 방 Task에서 가져옴
+                    # TODO 이 때, 작업은 병렬화가 가능하여, 해당 슬롯만큼이나 넘치게 작업을 가져와야 함.
+                    # TODO 가져오는 작업은 작은 시간 단위 일련의 작업이거나, 크기가 큰 작업의 일부를 가져와야 함.
+                    # TODO 바로 작업을 시작하게 만듦
+                    # parallel_tasks = self.find_parallel_tasks(permutation, subtask.location)
+                    #         for parallel_task in parallel_tasks:
+                    #             permutation.remove(parallel_task)
+                    #             makespan = self.update_log_and_makespan(
+                    #                 parallel_task, surveillance_start_time, log, index
+                    #             )
+                    #             surveillance_start_time = makespan
+
+                else:
+                    raise ValueError(
+                        f"Constraint task {constraint_task} not found in log."
+                    )
+            return makespan
 
         if not permutation:
             if self.env.current_location != self.goal_location:
@@ -140,7 +176,16 @@ class ExhaustiveSearch:
         subtask = permutation.pop(0)
 
         if subtask.constraints:
-            makespan = handle_constraints(subtask, makespan, index)
+            if subtask.constraints.get("Type", None) == "Waiting":
+                # Waiting 동작을 추가하고 makespan을 갱신하여 반환
+                makespan = handle_Waiting(subtask, makespan, index)
+            else:
+                # Surveillance 동안 수행할 병렬가능 작업을 배치
+                parallelable_subtask = self.get_parallelable_subtask(
+                    permutation, subtask
+                )
+
+                makespan = handle_Surveillance(subtask, makespan, index)
 
         makespan = update_log_and_makespan(subtask, makespan, index)
 
@@ -156,8 +201,15 @@ class ExhaustiveSearch:
             for subtask, info in self.schedule.items()
         ]
         df = pd.DataFrame(results).iloc[::-1]
-        print(df)
         return df
+
+    def get_parallelable_subtask(self, permutation, target_subtask):
+        parallelable_subtasks = [
+            subtask
+            for subtask in permutation
+            if subtask.location == target_subtask.location
+        ]
+        return parallelable_subtasks
 
 
 def get_permutations(lists, result):
