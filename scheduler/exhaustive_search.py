2e6f4c1038ef62178bd7d6ab4f83b462d332d2bf
do-dong-park <rnlgksclsrn9868@gmail.com>
2024-07-04 17:03:34 +0900

    🎨 1차 리펙토링

--- scheduler/exhaustive_search.py
+++ scheduler/exhaustive_search.py
@@ -8,7 +8,6 @@ from concept.task import Subtask, Task, get_all_subtasks
 
 class ExhaustiveSearch:
     def __init__(self, env: Env, tasks: list[Task]) -> None:
-        """Initialize the TaskScheduler with environment and tasks"""
         self.env = env
         self.init_location = self.env.current_location
         self.goal_location = self.env.goal_location
@@ -17,13 +16,11 @@ class ExhaustiveSearch:
 
     def init_tree(self):
         best_schedule = None
-        best_schedules = []
         best_cost = float("inf")
         subtasks = get_all_subtasks(self.tasks, mode="all")
         permutations = list(get_permutations(subtasks, []))
 
-        # Calculate the optimal makespan
-        for idx, permutation in enumerate(permutations):
+        for permutation in permutations:
             temp_permutation = copy.deepcopy(permutation)
             self.env.current_location = self.init_location
             try:
@@ -33,7 +30,6 @@ class ExhaustiveSearch:
             if total_cost < best_cost:
                 best_cost = total_cost
                 best_schedule = schedule
-                best_schedules.append(best_schedules)
 
         if best_schedule:
             return best_schedule
@@ -43,34 +39,21 @@ class ExhaustiveSearch:
     def exhaustive_search(
         self, permutation: list[Subtask], makespan: int, log: dict, index: int = 0
     ) -> tuple[int, dict]:
-        """Calculate the expected duration for the given permutation (without actually moving rooms)
-
-        Args:
-            permutation (list[Subtask]): Sequence of subtasks
-            makespan (int): Time taken to handle all subtasks
-            log (dict): Schedule considering wait and move
-            index (int, optional): Prevent log_dict key overwrite. Defaults to 0.
-
-        Returns:
-            tuple[int, dict]: Final makespan, log
-        """
 
         def update_log_and_makespan(subtask, start_time, index):
             transition_cost = self.env.get_cost(subtask.location)
             move_start_time = start_time
             move_end_time = move_start_time + transition_cost
 
-            # Log the move first
             if self.env.current_location != subtask.location:
                 log[
-                    f"Move_from_{self.env.current_location}_to_{subtask.location}_{index}"
+                    f"Move_{self.env.current_location}_to_{subtask.location}_{index}"
                 ] = {
                     "Start": move_start_time,
                     "End": move_end_time,
                 }
                 self.env.current_location = subtask.location
 
-            # Now log the subtask
             task_start_time = move_end_time
             task_end_time = task_start_time + subtask.duration
             log[f"{subtask.name}_{index}"] = {
@@ -80,20 +63,7 @@ class ExhaustiveSearch:
 
             return task_end_time
 
-        def handle_Waiting(subtask: Subtask, makespan: int, index: int) -> int:
-            """작업 시작 조건 충족위한 대기시간 결정
-
-            Args:
-                subtask (Subtask): 시작에 시간 제약이 있는 작업
-                makespan (int): 작업 시작 전 시각
-                index (int): dict key 겹침 방지용
-
-            Raises:
-                ValueError: Constraint가 없는 작업이 해당 함수에 들어온 경우
-
-            Returns:
-                int: 해당 작업 완료 시각
-            """
+        def handle_waiting(subtask: Subtask, makespan: int, index: int) -> int:
             constraint_task = subtask.constraints.get("After")
             constraint_duration = subtask.constraints.get("Duration", 0)
 
@@ -101,15 +71,11 @@ class ExhaustiveSearch:
                 constraint_key = next(
                     (key for key in log if key.startswith(constraint_task)), None
                 )
-                # constraint_key : 제약 조건 기준 작업
                 if constraint_key:
                     task_end_time = log[constraint_key]["End"]
-
                     if makespan >= task_end_time + constraint_duration:
-                        # 만약, 현 시점이 제약 조건을 충족하였다면
-                        return makespan  # No waiting needed
+                        return makespan
                     else:
-                        # 제약 조건을 충족하지 않는 경우, Waiting
                         waiting_time = task_end_time + constraint_duration - makespan
                         log[f"Waiting_for_{constraint_task}_{index}"] = {
                             "Start": makespan,
@@ -117,32 +83,17 @@ class ExhaustiveSearch:
                         }
                         return makespan + waiting_time
                 else:
-                    print(f"에러 당시 task : {subtask}")
-                    print(f"에러 당시 로그 : {log}")
                     raise ValueError(
                         f"Constraint task {constraint_task} not found in log."
                     )
             return makespan
 
-        def handle_Surveillance(
+        def handle_surveillance(
             subtask: Subtask,
             parallelable_subtasks: list[Subtask],
             makespan: int,
             index: int,
         ) -> int:
-            """Surveillance Subtask와 함께 병렬화를 진행할 작업을 선정
-
-            Args:
-                subtask (Subtask): 시작에 시간 제약이 있는 작업
-                makespan (int): 작업 시작 전 시각
-                index (int): dict key 겹침 방지용
-
-            Raises:
-                ValueError: Constraint가 없는 작업이 해당 함수에 들어온 경우
-
-            Returns:
-                int: 해당 작업 완료 시각
-            """
             constraint_task = subtask.constraints.get("After")
             constraint_duration = subtask.constraints.get("Duration", 0)
 
@@ -150,10 +101,8 @@ class ExhaustiveSearch:
                 constraint_key = next(
                     (key for key in log if key.startswith(constraint_task)), None
                 )
-                # constraint_key : 제약 조건 기준 작업
                 if constraint_key:
                     precedence_task_end_time = log[constraint_key]["End"]
-
                     if makespan == precedence_task_end_time + constraint_duration:
                         task_start_time = makespan
                         for parallelable_subtask in parallelable_subtasks:
@@ -168,7 +117,7 @@ class ExhaustiveSearch:
                         return makespan
                     else:
                         raise ValueError(
-                            f"The surveillance task must start right after precedence task end"
+                            "The surveillance task must start right after precedence task end"
                         )
                 else:
                     raise ValueError(
@@ -179,12 +128,11 @@ class ExhaustiveSearch:
         if not permutation:
             if self.env.current_location != self.goal_location:
                 log[
-                    f"Move_from_{self.env.current_location}_to_{self.goal_location}_{index}"
+                    f"Move_{self.env.current_location}_to_{self.goal_location}_{index}"
                 ] = {
                     "Start": makespan,
                     "End": makespan + self.env.get_cost(self.goal_location),
                 }
-
                 makespan += self.env.get_cost(self.goal_location)
             return makespan, log
 
@@ -192,21 +140,14 @@ class ExhaustiveSearch:
 
         if subtask.constraints:
             if subtask.constraints.get("Type", None) == "Waiting":
-                # Waiting 동작을 추가하고 makespan을 갱신하여 반환
-                makespan = handle_Waiting(subtask, makespan, index)
+                makespan = handle_waiting(subtask, makespan, index)
             else:
-                # Surveillance 동안 수행할 병렬가능 작업을 배치
                 permutation, parallelable_subtasks = self.get_parallelable_subtask(
                     permutation, subtask
                 )
-                try:
-                    makespan = handle_Surveillance(
-                        subtask, parallelable_subtasks, makespan, index
-                    )
-                except ValueError:
-                    raise ValueError(
-                        "The surveillance task must start right after precedence task end"
-                    )
+                makespan = handle_surveillance(
+                    subtask, parallelable_subtasks, makespan, index
+                )
 
         makespan = update_log_and_makespan(subtask, makespan, index)
 
@@ -225,9 +166,6 @@ class ExhaustiveSearch:
         return df
 
     def get_parallelable_subtask(self, permutation, target_subtask):
-        # Surveillance 15분. 결과값은 15분 자르기 + alpha겠지?
-
-        # 앞으로 해야 하는 부엌 작업
         parallelable_subtasks = [
             subtask
             for subtask in permutation
@@ -235,18 +173,15 @@ class ExhaustiveSearch:
             and target_subtask.task_name != subtask.task_name
         ]
 
-        # 병렬 가능 후보군에서 해당 기간동안 병렬화할 수 있는 작업만 선택
         cumulative_duration = 0
         splitted_subtasks = []
         is_parallel_complete = False
 
-        # 사이즈 20에 맞게 잘라야 함.
         for parallelable_subtask in parallelable_subtasks:
             cumulative_duration += parallelable_subtask.duration
 
             if cumulative_duration > target_subtask.duration:
                 if not is_parallel_complete:
-
                     over_duration = cumulative_duration - target_subtask.duration
 
                     splitted_subtask1 = copy.deepcopy(parallelable_subtask)
@@ -259,14 +194,11 @@ class ExhaustiveSearch:
                     is_parallel_complete = True
                 else:
                     splitted_subtasks.append(parallelable_subtask)
-
             else:
                 splitted_subtasks.append(parallelable_subtask)
 
-        # 병렬처리할 작업
         parallelable_subtasks = []
         cumulative_duration = 0
-        # 병렬 처리 이후, 남겨진 수행할 작업
         left_splitted_subtasks = []
         is_parallel_complete = False
 
@@ -274,30 +206,21 @@ class ExhaustiveSearch:
             if not is_parallel_complete:
                 cumulative_duration += splitted_subtask.duration
                 parallelable_subtasks.append(splitted_subtask)
-
                 if cumulative_duration == target_subtask.duration:
                     is_parallel_complete = True
             else:
                 left_splitted_subtasks.append(splitted_subtask)
 
-        # 기존 permutation에서 병렬 처리할 작업은 빼고, 남은 작업으로 작업 업데이트
         result_permutation = []
-
         for subtask in permutation:
             is_parallel_subtask = False
-            # 병렬처리 대상 작업은 기존 permutation에서 제거
             for parallelable_subtask in parallelable_subtasks:
                 if subtask.name == parallelable_subtask.name:
                     is_parallel_subtask = True
 
-            # 나머지 작업이면 업데이트된 작업으로 바꿔치기
             for left_splitted_subtask in left_splitted_subtasks:
                 if subtask.name == left_splitted_subtask.name:
                     result_permutation.append(left_splitted_subtask)
-
-            # 일반적 경우 그냥 추가
-            if is_parallel_subtask:
-                continue
 
             if not is_parallel_subtask:
                 result_permutation.append(subtask)
@@ -306,14 +229,12 @@ class ExhaustiveSearch:
 
 
 def get_permutations(lists, result):
-    # If all lists are exhausted, return the result
     if all(len(lst) == 0 for lst in lists):
         yield result
         return
 
-    # Choose an element from each list
     for i, lst in enumerate(lists):
-        if lst:  # Only if the list has elements
+        if lst:
             new_result = result + [lst[0]]
             new_lists = [lst[1:] if j == i else lst for j, lst in enumerate(lists)]
             yield from get_permutations(new_lists, new_result)
