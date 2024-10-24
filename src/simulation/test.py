0e33d340d2b2b77164f3bda8084c5a2b82859a1f
do-dong-park <rnlgksclsrn9868@gmail.com>
2024-10-24 17:59:33 +0900

    로거 추가

--- src/simulation/test.py
+++ src/simulation/test.py
@@ -1,16 +1,36 @@
+import logging
 import os
 
 import omnigibson as og
 import torch as th
 import yaml
+from omnigibson.action_primitives.action_primitive_set_base import (
+    ActionPrimitiveErrorGroup,
+)
 from omnigibson.action_primitives.starter_semantic_action_primitives import (
     StarterSemanticActionPrimitives,
     StarterSemanticActionPrimitiveSet,
 )
 from omnigibson.macros import gm
+from omnigibson.utils.ui_utils import create_module_logger
 
 gm.USE_GPU_DYNAMICS = False
 gm.ENABLE_FLATCACHE = True
+
+log = create_module_logger(module_name=__name__)
+
+console_handler = logging.StreamHandler()
+console_formatter = logging.Formatter("%(name)s - %(levelname)s - %(message)s")
+console_handler.setFormatter(console_formatter)
+
+file_handler = logging.FileHandler("./src/simulation/logs/app.log", "a")
+file_formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+file_handler.setFormatter(file_formatter)
+
+log.addHandler(console_handler)
+log.addHandler(file_handler)
 
 
 def execute_controller(ctrl_gen, env):
@@ -27,7 +47,11 @@ def main():
     config = yaml.load(open(config_filename, "r"), Loader=yaml.FullLoader)
 
     # Update it to run a grocery shopping task
-    config["scene"]["not_load_object_categories"] = ["ceilings", "pot_plant"]
+    config["scene"]["not_load_object_categories"] = [
+        "ceilings",
+        "pot_plant",
+        "straight_chair",
+    ]
     config["scene"]["load_room_types"] = ["living_room"]
     config["scene"]["load_room_instances"] = ["living_room_0"]
     config["objects"] = [
@@ -48,25 +72,28 @@ def main():
 
     # Allow user to move camera more easily
     controller = StarterSemanticActionPrimitives(env, enable_head_tracking=False)
-    sofa = scene.object_registry("name", "ottoman_ycfbsd_0")
+    table = scene.object_registry("name", "breakfast_table_skczfi_0")
     apple = scene.object_registry("name", "apple")
 
-    # Grasp apple
-    print("Executing controller")
+    try:
+        # Grasp apple
+        print("Executing controller")
 
-    execute_controller(
-        controller.apply_ref(StarterSemanticActionPrimitiveSet.GRASP, apple), env
-    )
-    print("Finished executing grasp")
+        execute_controller(
+            controller.apply_ref(StarterSemanticActionPrimitiveSet.GRASP, apple), env
+        )
+        print("Finished executing grasp")
 
-    # Place on sofa
-    print("Executing controller")
+        # Place on sofa
+        # print("Executing controller")
 
-    execute_controller(
-        controller.apply_ref(StarterSemanticActionPrimitiveSet.PLACE_ON_TOP, sofa),
-        env,
-    )
-    print("Finished executing place")
+        # execute_controller(
+        #     controller.apply_ref(StarterSemanticActionPrimitiveSet.PLACE_ON_TOP, table),
+        #     env,
+        # )
+        print("Finished executing place")
+    except ActionPrimitiveErrorGroup as e:
+        log.error(f"Failed to execute action primitives: {e}")
 
     # Always shut down the environment cleanly at the end
     og.clear()
