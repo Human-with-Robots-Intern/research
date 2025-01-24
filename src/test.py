8dcf575f2ee5fcc74667e5b41e588fa343341f57
esom08 <doll010808@gmail.com>
2025-01-24 13:42:19 +0900

    refactor: remove unused HMM implementation and update task choice in test script

--- src/test.py
+++ src/test.py
@@ -60,7 +60,7 @@ def main():
         controller = init_ai2thor()
 
     task_files = list_task_files()
-    task_file_name = get_user_task_choice(task_files, choice=2)
+    task_file_name = get_user_task_choice(task_files, choice=1)
 
     # Load the chosen task data
     task_data = load_task_data_from_file(task_file_name)
