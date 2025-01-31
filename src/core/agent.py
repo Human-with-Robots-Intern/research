ff02b7205247023798d28f2f19cc8436810a01b2
okjy89 <okjy89@hanyang.ac.kr>
2025-01-31 12:35:13 +0900

    monitering에 waiting 추가

--- src/core/agent.py
+++ src/core/agent.py
@@ -76,37 +76,58 @@ class Agent:
         except Exception as e:
             raise Exception(f"Error saving knowledge: {e}")
 
-    def monitering_timing(plan_about_time_critical):
+    def monitering_timing(self, subtasks, urgency_time):
         # plan_about_time_critical : time-critical에 대한 planning
+        # subtasks : subtask 묶음. 형식은 dictionary {"subtask이름" : 시간}
+        # urgency_time : urgency 시간(숫자)
         # 0.7 : monitering의 기준 timing
-        # 0.7이 되는 부분의 subtask 파악.
-        # 0.7 기준 그 전에 오는 subtask만 추가.
-        # 시간 넘으면 monitering 붙이기.
-        plan_about_time_critical = {"subtask1":3,"subtask2":4} ##예시_이거에 맞춰서 입력 형식 보내거나 두 값을 보내주어야 함.
+        # monitering_time은 기존 전체 시간의 0.7배
+        # monitering_time이 지나기 전까지의 subtask들을 붙인다.
+        # wait는 원래 subtask의 urgency에 맞춰서 채워넣기.
+        subtasks = {
+            "navigate": 3,
+            "pickup": 2,
+            "drop": 5
+        }
+        urgency_time = 10
+        ####################################
         time_sum = 0
-        subtask = list(plan_about_time_critical.keys())
-        time = list(plan_about_time_critical.values())
-        monitering_time = 0.7 * sum(time)
-        replanning_list = []
+        subtask_during_time_critical = list(subtasks.key())
+        time_during_time_critical = list(subtasks.value())
+        monitering_time = 0.7 * urgency_time
+        with_monitering_list = []
+        with_monitering_time_list = []
 
-        for t in range(len(plan_about_time_critical)):
-            time_sum += time[t]
-            if time_sum > monitering_time:
-                replanning_list.append("monitering")
-                return replanning_list
-            else:
-                replanning_list.append(subtask[t])
+        if subtask_during_time_critical == ["navigate"]:
+            with_monitering_list.append(subtask_during_time_critical[0])
+            with_monitering_time_list.append(time_during_time_critical[0])
+            with_monitering_list.append("waiting")
+            with_monitering_time_list.append(urgency_time-time_during_time_critical[0])
 
-        return replanning_list
+        else:
+            for t in range(len(time_during_time_critical)):
+                time_sum += time_during_time_critical[t]
+                if time_sum > monitering_time:
+                    with_monitering_list.append("monitering")
+                    with_monitering_time_list.append(0.1)
+                    with_monitering = dict(zip(with_monitering_list, with_monitering_time_list))
+                    return with_monitering
+                else:
+                    with_monitering_list.append(subtask_during_time_critical[t])
+                    with_monitering_time_list.append(time_during_time_critical[t])
 
-    def bayesian_estimate(self, actual_duration: float, subtask):
+        with_monitering = dict(zip(with_monitering_list, with_monitering_time_list))
+
+        return with_monitering
+    
+
+    def bayesian_estimate(self, actual_duration: float, subtask_name):
         # actual_duration : monitering한 시간
         # ground_truth : 해당 subtask의 ground_truth
         # prior_mean/variance : 이전에 예상한 값의 분포
         # cooking_data : subtask의 진행정도 // 여기에 noise를 주어야 한다.
         # posterior_mean/variance : cooking_data를 받은 후 bayesian estimate를 통해 도출된 새로운 예상한 값의 분포.
         # knowledge.json 파일에서 불러오고 업데이트.
-        subtask_name = subtask
 
         ground_truth = 10  # 나중에 subtask 이름에 따른 값으로 ground_truth.json 파일에서 불러와야 함.
         estimate_load = self._load_knowledge(KNOWLEDGE_PATH)
