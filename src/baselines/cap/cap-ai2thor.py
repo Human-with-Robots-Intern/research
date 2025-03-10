d4366ca940c088453ed71741513f22227bf2ac0b
okjy89 <okjy89@hanyang.ac.kr>
2025-03-10 22:39:00 +0900

    모듈위치 차이로 인해 발생한 경로문제 해결 Navi, Act의 class에 전달되는 인자 과다(수정하면서 줄어든듯) 해결 act 에 전달되는 log_file 문제을 다른 방식으로 수정.
    이전과 달라진 행동 이름들 e.g., move_to -> move_in_direction 등 을 지금 버전에 맞게 수정

--- src/baselines/cap/cap-ai2thor.py
+++ src/baselines/cap/cap-ai2thor.py
@@ -3,24 +3,24 @@ import numpy as np
 import time
 import threading
 import copy
-
+import logging
 
 # import for ai2thor
 from ai2thor.controller import Controller
 
 # setting.json 에 ai2thor 위치 환경변수 추가한 상태로 해야함.
-from handlers.camera_handler import CameraHandler
-from handlers.navigation_handler import NavigationHandler
-from handlers.action import Action
+from ithor.handlers.camera_handler import CameraHandler
+from ithor.handlers.navigation_handler import NavigationHandler
+from ithor.handlers.action import Action
 
-from utils.constants import *
-from utils.file_utils import *
+from ithor.utils.constants import *
+from ithor.utils.file_utils import *
 
 from pygments import highlight
 from pygments.lexers import PythonLexer
 from pygments.formatters import TerminalFormatter
 
-import utils.LMPgen as gen
+import baselines.cap.utils.LMPgen as gen
 
 
 def initialize_controller(log_file):
@@ -39,18 +39,22 @@ def initialize_controller(log_file):
         fieldOfView=60,
     )
     camera_handler = CameraHandler(controller)
-    Navi = NavigationHandler(controller, camera_handler, log_file)
-    Act = Action(controller, Navi, camera_handler, log_file)
+    Navi = NavigationHandler(controller)
+
+    logging.basicConfig(level=logging.DEBUG)
+    logger = logging.getLogger(__name__)
+
+    Act = Action(controller, log=logger)
 
     return controller, camera_handler, Navi, Act
 
 
 ## LMP Prompts
 # 텍스트 파일 읽기
-prompt_scene_ui_path = "cap/data/prompt_scene_ui.txt"
-prompt_parse_obj_name_path = "cap/data/prompt_parse_obj_name.txt"
-prompt_parse_question_path = "cap/data/prompt_parse_question.txt"
-prompt_fgen_path = "cap/data/prompt_fgen.txt"
+prompt_scene_ui_path = "src/baselines/cap/data/prompt_scene_ui.txt"
+prompt_parse_obj_name_path = "src/baselines/cap/data/prompt_parse_obj_name.txt"
+prompt_parse_question_path = "src/baselines/cap/data/prompt_parse_question.txt"
+prompt_fgen_path = "src/baselines/cap/data/prompt_fgen.txt"
 
 
 def read_txt(file_path):
@@ -155,7 +159,7 @@ def setup_LMP(controller, Navi, Action, cfg_scene):
     for var_name, var_value in fixed_vars.items():
         vars_log.write(f"{var_name}: {var_value}\n")
 
-    variable_vars = {k: getattr(Navi, k) for k in ["move_to"]}
+    variable_vars = {k: getattr(Navi, k) for k in ["move_in_direction"]}
     variable_vars.update(
         {
             k: getattr(Action, k)
@@ -164,8 +168,8 @@ def setup_LMP(controller, Navi, Action, cfg_scene):
                 "slice",
                 "put",
                 "drop",
-                "toggleon",
-                "toggleoff",
+                "toggle_on",
+                "toggle_off",
                 "open",
                 "close",
             ]
@@ -224,7 +228,7 @@ if __name__ == "__main__":
         "Heat potato with microwave, wash a plate three times and cook fried egg"
     )
 
-    log_file = open(f"cap/result/cap_logs_{user_input}.txt", "w", buffering=1)
+    log_file = open(f"src/baselines/cap/result/cap_logs_{user_input}.txt", "w", buffering=1)
     controller, camera_handler, Navi, Acttion = initialize_controller(log_file)
 
     lmp_scene_ui = setup_LMP(controller, Navi, Acttion, cfg_scene)
