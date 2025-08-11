bd5aee84c6d18029052212676e724d3d63ac7a8e
hanyangtae <rudxo1997@hanyang.ac.kr>
2025-08-11 14:03:52 +0900

    우분투 밀어서 아마 커밋 다시하는거같음

---
+++ temp.py
@@ -0,0 +1,72 @@
+from robot_manager_interface.srv import RobotManager
+import sys
+import rclpy
+from rclpy.node import Node
+import csv
+from rclpy.callback_groups import MutuallyExclusiveCallbackGroup, ReentrantCallbackGroup
+from rclpy.executors import MultiThreadedExecutor
+
+
+class RobotManagerClient(Node):
+
+    def __init__(self):
+        super().__init__('robot_manager_client')
+        self.cli_h = self.create_client(RobotManager, '/robot_command') 
+        # self.cli_j = self.create_client(RobotManager, 'j100_0638/robot_command')  
+        # self.cli_u = self.create_client(RobotManager, 'ur5/robot_command') 
+        while not self.cli_h.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('service not available, waiting again...')
+        # while not self.cli_j.wait_for_service(timeout_sec=1.0):
+        #     self.get_logger().info('service not available, waiting again...')
+        # while not self.cli_u.wait_for_service(timeout_sec=1.0):
+        #     self.get_logger().info('service not available, waiting again...')
+        # self.client_entity_list = [self.cli_h, self.cli_j, self.cli_u]
+        self.client_entity_list = [self.cli_h]
+
+    def send_request(self, robot_model, instruction, A, B):
+        req_temp = RobotManager.Request()
+        req_temp.a = robot_model
+        req_temp.b = instruction
+        req_temp.c = A
+        req_temp.d = B
+        future = self.client_entity_list[robot_model].call_async(req_temp)
+        return future
+
+def main(args=None):
+    rclpy.init(args=args)
+    robot_manager_service_client = RobotManagerClient()
+    with open('planner_instruction/instruction001.csv', mode='r', newline='') as instruction_file:
+        next(csv.reader(instruction_file))
+        for row in csv.reader(instruction_file):
+            try:
+                robot_model = int(row[0])
+                instruction = int(row[1])
+                A = int(row[2]) if row[2] else 100
+                B = int(row[3]) if row[3] else 100
+                future = robot_manager_service_client.send_request(robot_model, instruction, A, B)
+                while rclpy.ok():
+                    rclpy.spin_once(robot_manager_service_client)
+                    if future.done():
+                        try:
+                            response = future.result()
+                        except Exception as e:
+                            robot_manager_service_client.get_logger().info(
+                                'Service call failed %r' % (e,))
+                            sys.exit('종료')
+                        else:
+                            if response.success == True:
+                                robot_manager_service_client.get_logger().info(
+                                    'Result = successed')  # CHANGE
+                            else:
+                                robot_manager_service_client.get_logger().info(
+                                    'Result = failed')  # CHANGE
+                                sys.exit('종료')
+                        break
+            except ValueError as e:
+                print(f"Skipping invalid row: {row}, Error: {e}")
+    robot_manager_service_client.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
