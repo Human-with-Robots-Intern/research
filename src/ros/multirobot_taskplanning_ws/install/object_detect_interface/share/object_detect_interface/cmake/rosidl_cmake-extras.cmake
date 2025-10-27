a836f67151a83d55433bfcbb69466f93654180c2
hanyangtae <rudxo1997@hanyang.ac.kr>
2025-10-27 18:00:08 +0900

    ros 폴더 경로 바꿈

--- src/ros/multirobot_taskplanning_ws/install/object_detect_interface/share/object_detect_interface/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(object_detect_interface_IDL_FILES "msg/ObjectData.idl;srv/DetectObjects.idl")
-set(object_detect_interface_INTERFACE_FILES "msg/ObjectData.msg;srv/DetectObjects.srv;srv/DetectObjects_Request.msg;srv/DetectObjects_Response.msg")
