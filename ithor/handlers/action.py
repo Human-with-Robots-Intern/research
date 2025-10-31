08c5d12fa00fc7ac948076b2ba1cf3994b7a5bb4
do-dong-park <rnlgksclsrn9868@gmail.com>
2025-10-31 22:18:04 +0900

    시뮬 소요 시간 단축

--- ithor/handlers/action.py
+++ ithor/handlers/action.py
@@ -490,7 +490,7 @@ class Action:
         # 카메라 각도 조정
         self.navi.adjust_camera_to_object(object_id)
         self.success_log(result, f"adjust camera to {object_id} for monitoring action")
-        time.sleep(MONITORING_DURATION)
+        time.sleep(ACTION_TIME_SLEEP)
 
         # 원위치로 회전
         if degree != 0:
@@ -502,7 +502,7 @@ class Action:
                 else:
                     self.controller.step(action="RotateRight", degrees=step_deg)
 
-        time.sleep(MONITORING_DURATION)
+        time.sleep(ACTION_TIME_SLEEP)
         return MONITORING_DURATION
 
     def wait(self, wait_time: float):
