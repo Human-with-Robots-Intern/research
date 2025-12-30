f974496582a0f72eaf63aed039db1361492b4381
do-dong-park <rnlgksclsrn9868@gmail.com>
2025-12-30 12:55:47 +0900

    실행 instruction default 값 변경

--- src/dag_bayesian.py
+++ src/dag_bayesian.py
@@ -67,13 +67,13 @@ def parse_arguments():
     parser.add_argument(
         "--instruction",
         type=str,
-        default="01_boil_potato_and_heat_the_bread_using_microwave.json",
+        default="10_make_a_coffee_and_put_apple_and_lettuce_in_fridge.json",
         help="실행할 태스크 instruction 문자열 또는 번호 (default: None)",
     )
     parser.add_argument(
         "--case",
         type=str,
-        default="tasks_2_constraints_2",
+        default="tasks_2_constraints_1",
         help="The name of the case.",
     )
     parser.add_argument(
@@ -139,7 +139,7 @@ def parse_arguments():
     parser.add_argument(
         "--beam_width",
         type=int,
-        default=5,
+        default=3,
         help="Scheduler beam width (기본값: constants.py 값)",
     )
     parser.add_argument(
